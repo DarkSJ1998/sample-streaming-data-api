1a0bd3217b8995d14efaf37e64cba83b8aa18365
DarkSJ1998 <sarthakjn12@gmail.com>
2021-07-02 20:07:21 +0530

    Generated using Spring Initializr

---
+++ src/main/java/com/darksj1998/samplestreamingdataapi/SampleStreamingDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.darksj1998.samplestreamingdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleStreamingDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleStreamingDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darksj1998/samplestreamingdataapi/SampleStreamingDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darksj1998.samplestreamingdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleStreamingDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
